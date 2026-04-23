45bb8a15de39cf3b284ac8fa3807449b611f969e
Max Allen <125095090+AzureToTheMax@users.noreply.github.com>
2026-04-23 18:59:37 -0400

    V1.1
    Hopefully this corrects the formatting and comparison issues

--- CapabilityAccessManager Cleanup/CapabilityAccessManager-Detection.ps1
+++ CapabilityAccessManager Cleanup/CapabilityAccessManager-Detection.ps1
@@ -20,23 +20,24 @@ Updated:
             
 Version history:
 1 -  4/22/2026 - Initial public release of script.
+1.1 - 4/23/2026 - corrected formatting issue that caused the comparison to sometimes incorrectly calculate (would report as over 1GB when well under it)
 
 #>
 
 #Max file size in MB
 #Note, as of writing, the expected size is not yet known. The current median is 550 MB.
-$MaxSize = 1024
+$MaxSize = 1073741824 #1 GB in bytes. Yes, leave it as bytes.
 
 #Get the size of the DB file
-$DBSize = "{0:N2}" -f ((Get-Item "C:\ProgramData\Microsoft\Windows\CapabilityAccessManager\CapabilityAccessManager.DB-wal").Length / 1MB)
+$DBSize = (Get-Item "C:\ProgramData\Microsoft\Windows\CapabilityAccessManager\CapabilityAccessManager.DB-wal").Length
 
-#If the size is over $MaxSize MB (default is 1024 MB, or 1 GB), exit with failure.
-if($DBSize -ge $MaxSize){
+#If the size is over $MaxSize MB (default is 1024 MB, or 1 GB), exit with failure. Leave it as gt. PowerShell doesn't like eq, including ge, with decimals apparently, so leave it as.
+if($DBSize -gt $MaxSize){
     #File is over max size
-    write-host "$DBSize" #Note that the write-host lacks anything but the data value (no "MB", etc). When exported, you simply need to know this is in MB. This is done for easy Excel sorting.
+    write-host "$("{0:N2}" -f ($DBSize / 1MB))" #Note that the write-host lacks anything but the data value (no "MB", etc). When exported, you simply need to know this is in MB. This is done for easy Excel sorting.
     exit 1
 } else {
     #File is under max size
-    write-host "$DBSize" #Note that the write-host lacks anything but the data value (no "MB", etc). When exported, you simply need to know this is in MB. This is done for easy Excel sorting.
+    write-host "$("{0:N2}" -f ($DBSize / 1MB))" #Note that the write-host lacks anything but the data value (no "MB", etc). When exported, you simply need to know this is in MB. This is done for easy Excel sorting.
     exit 0
 }
